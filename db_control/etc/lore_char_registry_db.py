0a42b46dbd7c11981d0cba75393cead8932d21ec
themanyfaceddemon <neko-d-05@mail.ru>
2026-01-23 09:31:33 +0300

    not ready lorechar_db

--- db_control/etc/lore_char_registry_db.py
+++ db_control/etc/lore_char_registry_db.py
@@ -1,7 +1,31 @@
-# TODO:
 
-# name
-# how pick credential.id
-# game_char_bd_id
-# status
-# wiki_url
+from queue import Queue
+
+from ..base_db import BaseDB, SQLTask
+
+
+
+
+class LoreCharRegistryDB(BaseDB):
+    _db_name = "lore_char_registry"
+
+    _worker_started: bool = False
+    _queue = Queue()
+
+    @classmethod
+    def set_up(cls) -> None:
+        sql_t = [
+            SQLTask(
+                """
+                CREATE TABLE IF NOT EXISTS lore_char_registry (
+                    name TEXT PRIMARY KEY,
+                    status TEXT NOT NULL,
+                    who_pick INTEGER, 
+                    game_db_id INTEGER,
+                    wiki_url TEXT
+                );
+                """
+            ),
+        ]
+        super()._init_db(sql_t)
+    # TODO: methods
