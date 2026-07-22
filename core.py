e61b281db1dc3fbf571b88d97060cdc371560db8
themanyfaceddemon <gastermini357@gmail.com>
2026-07-22 14:40:39 +0300

    cleanup

--- core.py
+++ core.py
@@ -1,9 +1,6 @@
 import contextlib
 
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-from template_env import LOCAL_SETUP
 
 
 @contextlib.asynccontextmanager
@@ -17,10 +14,3 @@ app = FastAPI(
     redoc_url=None,
     openapi_url=None,
 )
-
-if LOCAL_SETUP:
-    app.mount(
-        "/static",
-        StaticFiles(directory="static"),
-        name="static",
-    )
